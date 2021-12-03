685bd972036fc3f94cccaa4aa05fe6d4a3f847e9
Josh Tregenza <josh@joshtregenza.com>
2021-12-03 11:59:58 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
+	generateBuildId: () => 'build',
 	reactStrictMode: true,
+	images: {
+	  disableStaticImages: true
+	},
+	webpack5: true,
+	webpack: (config) => {
+	  config.resolve.fallback = {
+		...config.resolve.fallback,
+		fs: false
+	  }
+  
+	  return config;
+	},
 };
