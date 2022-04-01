4888a446c7a468305ed3c92f6daac30c763940f6
Josh Tregenza <josh@joshtregenza.com>
2022-04-01 11:33:23 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ export default function Home() {
     <div className={styles.container}>
 
       <main className={styles.main}>
-      <div className="home-page">
+      <div className={styles.homePage}>
       <h1>Welcome to the website of Josh Tregenza</h1>
-      <article className="opening-blurb"><p><code>designer, storyteller, craftsman</code></p>
+      <article className={openingBlurb}><p><code>designer, storyteller, craftsman</code></p>
       <p>As a <code>designer</code>, it is my job to make sure that businesses are putting the wellbeing of people over profits, ego and consumption so that they can find greater success.</p>
       <p>As a <code>storyteller</code>, I bring people together and uplift them with the words, products and ideas that are told.</p>
       <p>As a <code>craftsman</code>, I seek to improvise with care and follow the delightfully absurd to appreciate considerate work.</p>
-      <p>I'm less of a visual designer and more of a systems designer. I build teams of designers, developers and product managers to work best together. I delve into the murky ends of databases and business process to make sense of technical debt and constraints that systems develop over time to then unpack complexity and build to it, making sure the most amount of people are accounted for.</p><p>I help build culture, to be caring, compassion and quirky to encourage the best work without sacrificing health and wellbeing. </p></article>
+      <p>I&apos;m less of a visual designer and more of a systems designer. I build teams of designers, developers and product managers to work best together. I delve into the murky ends of databases and business process to make sense of technical debt and constraints that systems develop over time to then unpack complexity and build to it, making sure the most amount of people are accounted for.</p><p>I help build culture, to be caring, compassion and quirky to encourage the best work without sacrificing health and wellbeing. </p></article>
       </div>
       </main>
 
