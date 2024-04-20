75c9e9540cc6539fb9262dfd2547614fcd390684
Josh Tregenza <josh@joshtregenza.com>
2024-04-20 13:38:30 +1000

    Update engagement.js

--- pages/engagement.js
+++ pages/engagement.js
@@ -1,7 +1,8 @@
 export default function Engagement() {
     return (
         <>
-        <div data-tf-live="01HVWS1TKM2W13G9R1W206AWHB"></div><script src="//embed.typeform.com/next/embed.js"></script>
+        <div data-tf-live="01HVWS1TKM2W13G9R1W206AWHB"></div>
+        <script async src="//embed.typeform.com/next/embed.js"></script>
         </>
     )
 }
